5c6ec2524614834004a50a277fda900102cd9a49
Chacalerks <erksmartinez2014@gmail.com>
2023-10-10 00:54:47 -0600

    Reservation RazorView
    Just the layout with html and css

--- CancherksWebApp/Pages/Index.cshtml.cs
+++ CancherksWebApp/Pages/Index.cshtml.cs
@@ -7,6 +7,7 @@ namespace CancherksWebApp.Pages
     {
         private readonly ILogger<IndexModel> _logger;
 
+        public string Message { get; set; }
         public IndexModel(ILogger<IndexModel> logger)
         {
             _logger = logger;
@@ -14,7 +15,7 @@ namespace CancherksWebApp.Pages
 
         public void OnGet()
         {
-
+            Message = "Hello World!";
         }
     }
 }
