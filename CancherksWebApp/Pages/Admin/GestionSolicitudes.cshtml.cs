7b172d353aa3df80b376791d0daadabe9dd17475
Chacalerks <erksmartinez2014@gmail.com>
2023-10-10 23:28:19 -0600

    the request is aceept complete

--- CancherksWebApp/Pages/Admin/GestionSolicitudes.cshtml.cs
+++ CancherksWebApp/Pages/Admin/GestionSolicitudes.cshtml.cs
@@ -1,7 +1,9 @@
+
 using CancherksWebApp.Data;
 using CancherksWebApp.Model;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Security.Cryptography;
 
 namespace CancherksWebApp.Pages.Admin
 {
@@ -19,12 +21,50 @@ namespace CancherksWebApp.Pages.Admin
         public List<Request> Requests { get; set; } // A�ade esta l�nea
 
 
-        public void OnGet()
+        public async Task OnGet()
         {
             role = HttpContext.Session.GetString("role");
             Installations = _context.Installation.ToList();
             Requests = _context.Request.Where(r => r.IdState == 1).ToList(); // Filtrar por idState == 1
+
+            Page();
+
         }
+
+        public async Task<IActionResult> OnPostAsync(string idSolicitud)
+        {
+            
+            var idRequest = Convert.ToInt32(idSolicitud);   
+            var modifyRequest = await _context.Request.FindAsync(idRequest);
+
+            if (modifyRequest == null)
+            {
+                Message = "Request not found";
+                Response.Redirect("/Admin/GestionSolicitudes");
+            }
+
+            modifyRequest.IdState = 2;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+                Message = "Request state has been updated";
+            }
+            catch (Exception ex)
+            {
+                Message = "Error while updating the request: " + ex.Message;
+            }
+
+            // Inicializa las listas después de actualizar
+            Installations = _context.Installation.ToList();
+            Requests = _context.Request.Where(r => r.IdState == 1).ToList(); // Filtrar por idState == 1
+
+            // Redirige a la misma página
+            return RedirectToPage("/Admin/GestionSolicitudes");
+        }
+
+
+
 
     }
 }
