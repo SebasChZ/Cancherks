6d80da1e751c64203e936010502626967930545a
Fernanda Murillo <murillomf2001@gmail.com>
2023-10-12 13:47:11 -0600

    activities loaded
    activities get loaded from database

--- CancherksWebApp/Pages/Admin/AgregarInstalacion.cshtml.cs
+++ CancherksWebApp/Pages/Admin/AgregarInstalacion.cshtml.cs
@@ -1,15 +1,28 @@
+using CancherksWebApp.Data;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using System.Data;
+using CancherksWebApp.Model;
 
 namespace CancherksWebApp.Pages.Admin
 {
     public class AgregarInstalacionModel : PageModel
     {
+        private readonly ApplicationDbContext _context;
+
         public string role { get; set; }
+
+        public AgregarInstalacionModel(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public List<Activity> Activities { get; set; }
+
         public void OnGet()
         {
             role = HttpContext.Session.GetString("role");
+            Activities = _context.Activity.ToList();
         }
     }
 }
